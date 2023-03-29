9b538d739d7facb38a0c941100a013642ec5be78
Huiying <huiying.zhenzhen@ucalgary.ca>
2023-03-28 19:50:43 -0600

    layout for task class

--- edu/ucalgary/oop/Task.java
+++ edu/ucalgary/oop/Task.java
@@ -8,5 +8,83 @@
 package edu.ucalgary.oop;
 
 abstract class Task {
-    
+    protected final int TASKID;
+    protected String description;
+    protected int duration;
+    protected int maxWindow;
+    protected Animal animal;
+
+    /** Constructor */
+
+    /**
+     * 
+     * @param taskId
+     * @param description
+     * @param duration
+     * @param maxWindow
+     * @param animal
+     * @throws IllegalArgumentException
+     */
+    public Task(int taskId, String description, int duration, int maxWindow, Animal animal) throws IllegalArgumentException{
+
+    }
+
+    /** Getters */
+
+    /**
+     * 
+     * @return
+     */
+    public int getTaskId(){
+
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public String getDescription(){
+
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public int getDuration(){
+
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public int getMaxWindow(){
+
+    }
+
+    /** Setters */
+    /**
+     * 
+     * @param description
+     */
+    public void setDescription(String description){
+
+    }
+
+    /**
+     * 
+     * @param duration
+     */
+    public void setDuration(int duration){
+
+    }
+
+    /**
+     * 
+     * @param maxWindow
+     */
+    public void setMaxWindow(int maxWindow){
+
+    }
 }
