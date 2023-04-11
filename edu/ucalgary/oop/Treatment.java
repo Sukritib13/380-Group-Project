b28d7c73780bda0b7ddf245b2b4e57460dc1ab28
Rimal01 <113727006+Rimal01@users.noreply.github.com>
2023-04-10 23:38:44 -0600

    Update Treatment with documentation

--- edu/ucalgary/oop/Treatment.java
+++ edu/ucalgary/oop/Treatment.java
@@ -1,36 +1,109 @@
 /**
- * Class description
- * @author Sukriti Badhwar, Rita Boury, Rimal Rizvi, Huiying Zhen Zhen
- * @version 1.0
- * @since 1.0
- */
+The Treatment class represents a medical treatment that is given to an animal.
+It has four private fields: id, animalId, taskId, and startHour.
+@author Sukriti Badhwar, Rita Boury, Rimal Rizvi, Huiying Zhen Zhen
+@version 1.3
+@since 1.0
+*/
 
 package edu.ucalgary.oop;
 
 public class Treatment {
-    private int startHour;
-
-    /** Constructor */
+    private String id;
+    private String animalId;
+    private String taskId;
+    private String startHour;
+    
     /**
-     * 
-     * @param animalId
-     * @param taskId
-     * @param description
-     * @param duration
-     * @param maxWindow
-     * @param startHour
-     * @throws IllegalArgumentException
+     * Constructor for Treatment class
+     * @param id unique identifier for the treatment
+     * @param animalId unique identifier for the animal receiving the treatment
+     * @param taskId unique identifier for the task associated with the treatment
+     * @param startHour the hour at which the treatment starts
      */
-    public Treatment(int animalId, int taskId, String description, int duration, int maxWindow, int startHour) throws IllegalArgumentException{
-
-    }
-
-    /**
-     * 
-     * @return
-     */
-    public int getStartHout(){
-
+    public Treatment(String id, String animalId, String taskId, String startHour) {
+        if(Integer.parseInt(startHour) < 0 || Integer.parseInt(startHour) > 23){
+            throw new IllegalArgumentException("Invalid start hour.");
+        }
+        
+        this.id = id;
+        this.animalId = animalId;
+        this.taskId = taskId;
+        this.startHour = startHour;
     }
     
+    /**
+    * Getter method for the id field
+    * @return the unique identifier for the treatment
+    */
+    public String getId() {
+        return id;
+    }
+    
+    
+    
+    /**
+     * Setter method for the id field
+     * @param id the unique identifier for the treatment
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    
+     /**
+     * Getter method for the animalId field
+     * @return the unique identifier for the animal receiving the treatment
+     */
+    public String getAnimalId() {
+        return animalId;
+    }
+
+    
+    
+    /**
+     * Setter method for the animalId field
+     * @param animalId the unique identifier for the animal receiving the treatment
+     */
+    public void setAnimalId(String animalId) {
+        this.animalId = animalId;
+    }
+
+    
+     /**
+     * Getter method for the taskId field
+     * @return the unique identifier for the task associated with the treatment
+     */
+    public String getTaskId() {
+        return taskId;
+    }
+    
+    
+    
+    /**
+     * Setter method for the taskId field
+     * @param taskId the unique identifier for the task associated with the treatment
+     */
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+    
+    
+    /**
+     * Getter method for the startHour field
+     * @return the hour at which the treatment starts
+     */
+    public String getStartHour() {
+        return startHour;
+    }
+    
+
+     /**
+     * Setter method for the startHour field
+     * @param startHour the hour at which the treatment starts
+     */
+    public void setStartHour(String startHour) {
+        this.startHour = startHour;
+    }
+
 }
