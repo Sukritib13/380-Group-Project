cf248749e995d081409c4125a847177356f936f3
Huiying <113801170+huiyingzhen@users.noreply.github.com>
2023-04-10 23:37:32 -0600

    Update Animal.java

--- edu/ucalgary/oop/Animal.java
+++ edu/ucalgary/oop/Animal.java
@@ -1,67 +1,108 @@
 /**
- * Class description
- * @author Sukriti Badhwar, Rita Boury, Rimal Rizvi, Huiying Zhen Zhen
- * @version 1.0
- * @since 1.0
- */
-
+* The Animal class is an abstract class that has the attributes of ID, nickname, and species. 
+* It provides methods to get and set these attributes as well as methods for feeding and cleaning.
+* @author Sukriti Badhwar, Rita Boury, Rimal Rizvi, Huiying Zhen Zhen
+* @version 1.9
+* @since 1.0
+*/
 package edu.ucalgary.oop;
 
-public class Animal {
-    private final int ANIMALID;
-    private String animalNickname;
-    private String animalSpecies;
-
-    /** Constructor */
-    /**
-     * @param
-     * @throws
-     */
-    public Animal(int AnimalId, String animalNickname, String animalSpecies) throws IllegalArgumentException{
-    	this.ANIMALID = AnimalId;
-    	this.animalNickname = animalNickname;
-    	this.animalSpecies = animalSpecies;
-    }
-
-    /** Getters */
+abstract class Animal {
+    protected String id;
+    protected String nickname;
+    protected String species;
+    
 
     /**
-     * getter method that returns the animal ID 
-     * @return
+     * Constructor for the Animal class. Takes in an id, nickname, and species for the animal.
+     * @param id The unique identifier for the animal
+     * @param nickname The nickname of the animal
+     * @param species The species of the animal
      */
-    public int getANIMALID(){
-    	return ANIMALID;	
+    public Animal(String id, String nickname, String species) {
+        this.id = id;
+        this.nickname = nickname;
+        this.species = species;
+    }
+    
+    /** 
+     * Getter for the ID of the animal
+     * @return the ID of the animal
+     */
+    public String getId() {
+        return this.id;
     }
 
     /**
-     * getter method that returns the animal nickname
-     * @return
+     * Setter for the ID of the animal
+     * @param id
      */
-    public String getAnimalNickname(){
-    	return animalNickname;
-    }
-    /**
-     * getter method that returns the animal species
-     * @return
-     */
-    public String getAnimalSpecies(){
-    	return animalSpecies;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    /** Setters */
+    /**
+     * Getter for the nickname of the animal
+     * @return the nickname of the animal
+     */
+    public String getNickname() {
+        return this.nickname;
+    }
     
     /**
-     * 
+     * Setter for the nickname of the animal
      * @param nickname
      */
-    public void setAnimalNickname(String nickname){
-    	this.animalNickname = nickname;
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
     }
+    
     /**
-     * 
+     * Getter for the species of the animal
+     * @return the species of the animal
+     */
+    public String getSpecies() {
+        return this.species;
+    }
+    
+    /**
+     * Setter for the species of the animal
      * @param species
      */
-    public void setAnimalSpecies(String species){
-    	this.animalSpecies = species;
+    public void setSpecies(String species) {
+        this.species = species;
     }
+
+    /**
+     * Getter for the feeding hours of the animal
+     * @return the feeding hours of the animal
+     */
+    public int[] getFeedingHours() {
+        return null;
+    }
+    
+    /**
+     * Getter for the duration of clianing the cage of the animal.
+     * @return the duration of cleaning the cage of the animal
+     */
+    public int getCleaningDuration() {
+        return 0;
+    }
+    
+    /**
+     * Getter for the duration of feeding the animal.
+     * @return the duration of feeding the animal
+     */
+    public int getFeedingDuration() {
+        return 0;
+    }
+    
+    /**
+     * Getter for the preparation time required for feeding the animal.
+     * @return the preparation time required for feeding the animal
+     */
+    public int getPrepTime() {
+        return 0;
+    }
+
 }
